a468c34cef5f25d7e5702ed51efea37d42a5b318
John Doe <johndoe@email.com>
2025-03-19 16:20:19 +0100

    Idk

--- dist/carcatalog/dto/create-carcatalog.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class CreateCarcatalogDto {
-    vehicle: string;
-    type: string;
-    color: string;
-    fuel: string;
-    manufacturer: string;
-    mass: number;
-    imageUrl?: string;
-}
