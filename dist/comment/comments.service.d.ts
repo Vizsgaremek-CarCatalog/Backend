6eb501fd0a63a6a1e527a70461e0a71f8e884ef0
dohyoliver <75310042+dohyoliver@users.noreply.github.com>
2025-04-05 14:22:48 +0200

    Delete dist directory

--- dist/comment/comments.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaService } from '../prisma.service';
-import { CreateCommentDto } from './dto/create-comment.dto';
-export declare class CommentsService {
-    private readonly db;
-    constructor(db: PrismaService);
-    create(createCommentDto: CreateCommentDto, userId: number, carId: number): unknown;
-}
